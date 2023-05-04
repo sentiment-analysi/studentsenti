d19097a948b4e2a6338abc40f458c98ba67d3d31
sentiment-analysi <129092803+sentiment-analysi@users.noreply.github.com>
2023-05-04 13:13:22 +0530

    Update annapp1.py

--- annapp1.py
+++ annapp1.py
@@ -77,7 +77,8 @@ def main():
               sentiment1 = predict_sentiment(review1)
               sentiment2 = predict_sentiment(review2)
               sentiment3 = predict_sentiment(review3)
-              c.execute("INSERT INTO reviews (course_experience, sentiment1, instructor,sentiment2, material, sentiment3) VALUES (?, ?, ?, ?)", (review1,sentiment1, review2,sentiment2, review3, sentiment3))
+              c.execute("INSERT INTO reviews (course_experience, sentiment1, instructor, sentiment2, material, sentiment3) VALUES (?, ?, ?, ?, ?, ?)", (review1, sentiment1, review2, sentiment2, review3, sentiment3))
+
               c.commit()
 
 
