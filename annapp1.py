20f467783d3b16e1a9efe9b6932b77f97f28bbbc
sentiment-analysi <129092803+sentiment-analysi@users.noreply.github.com>
2023-05-04 15:58:46 +0530

    Update annapp1.py

--- annapp1.py
+++ annapp1.py
@@ -103,6 +103,24 @@ def show_sentiment_wise_analytics(reviews_df):
     ax.set_title('Sentiment Analysis Results')
 
     st.pyplot(fig)
+    
+    fig, ax = plt.subplots(figsize=(10,5))
+    sentiment_labels = ['Positive', 'Negative']
+    question_labels = ['Q1', 'Q2', 'Q3', 'Total']
+    pos_counts = [num_pos_reviewsfor1, num_pos_reviewsfor2, num_pos_reviewsfor3, totalnum_pos_reviews]
+    neg_counts = [num_neg_reviewsfor1, num_neg_reviewsfor2, num_neg_reviewsfor3, totalnum_neg_reviews]
+    x = np.arange(len(question_labels))
+    width = 0.35
+    ax.bar(x - width/2, pos_counts, width, label='Positive', color='green')
+    ax.bar(x + width/2, neg_counts, width, label='Negative', color='red')
+    ax.set_xticks(x)
+    ax.set_xticklabels(question_labels)
+    ax.legend()
+    ax.set_ylabel('Number of Reviews')
+    ax.set_xlabel('Questions')
+    ax.set_title('Sentiment Analysis Results')
+
+    st.pyplot(fig)
 
    
 
