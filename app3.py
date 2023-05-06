3283d53e4de9bc4c86ab565fbc16ad628ffc4d94
sentiment-analysi <129092803+sentiment-analysi@users.noreply.github.com>
2023-05-06 21:57:57 +0530

    Update app3.py

--- app3.py
+++ app3.py
@@ -149,27 +149,33 @@ def main():
     if not is_admin:
         # Create a form to collect reviews from multiple users
         with st.form(key='review_form'):
-          usn=st.text_input('Enter USN:  *')
-          name=st.text_input('Your Name:  *')
-          review1 = st.text_input('How was the course experience?  *')
-          review2 = st.text_input('Tell us about the instructor?  *')
-          review3 = st.text_input('Was the material provided useful?  *')
-          
+          usn = st.text_input('Enter USN:')
+          name = st.text_input('Your Name:')
+          review1 = st.text_input('How was the course experience?')
+          review2 = st.text_input('Tell us about the instructor?')
+          review3 = st.text_input('Was the material provided useful?')
           submitted = st.form_submit_button('Submit')
-          
+
           # Store the reviews in the database
           if submitted:
-            if not usn or not name or not review1 or not review2 or not review3:
-                st.error('Please fill in all fields.')
-            elif len(usn) != 10:
-                st.error('Incorrect USN. The USN should be 10 characters.')
-            else:
-                sentiment1 = predict_sentiment(review1)
-                sentiment2 = predict_sentiment(review2)
-                sentiment3 = predict_sentiment(review3)
-                c.execute("INSERT INTO reviews2 (usn,name,course_experience, sentiment1, instructor, sentiment2, material, sentiment3) VALUES (?, ?, ?, ?, ?, ?, ?, ?)", (usn, name, review1, sentiment1, review2, sentiment2, review3, sentiment3))
-                conn.commit()
-                st.success('Thank you for submitting your reviews.')
+              if not usn or not name or not review1 or not review2 or not review3:
+                  st.error('Please fill in all fields.')
+              elif len(usn) != 10:
+                  st.error('Incorrect USN format. Please enter a 10-character USN.')
+              else:
+                  c.execute("SELECT * FROM reviews1 WHERE usn=?", (usn,))
+                  existing_entry = c.fetchone()
+                  if existing_entry is not None:
+                      st.error('Entry with this USN already exists.')
+                  else:
+                      sentiment1 = predict_sentiment(review1)
+                      sentiment2 = predict_sentiment(review2)
+                      sentiment3 = predict_sentiment(review3)
+                      c.execute("INSERT INTO reviews1 (usn,name,course_experience, sentiment1, instructor, sentiment2, material, sentiment3) VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
+                                (usn, name, review1, sentiment1, review2, sentiment2, review3, sentiment3))
+                      conn.commit()
+                      st.success('Thank you for submitting your reviews.')
+
 
     else:
         # Check if user is logged in
